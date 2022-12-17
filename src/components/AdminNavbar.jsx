06ccba875f71e00b2766240f6ad74ad2b77c32d9
atmoo-wil <wilaga.atmo@gmail.com>
2022-12-17 13:32:35 +0700

    memproteksi seluruh menu admin dan menambahkan nama admin di adminNavbar

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -1,13 +1,63 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { IoLogOutOutline } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { HOME } from "../router";
 import "../pages/Beranda";
-import { hover } from "@testing-library/user-event/dist/hover";
+// import { hover } from "@testing-library/user-event/dist/hover";
+
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import { useHistory } from "react-router-use-history";
+import jwt_decode from "jwt-decode";
 
 const AdminNavbar = () => {
-  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
+  const history = useHistory();
+
+  const Logout = async () => {
+      try {
+          await axios.delete('http://localhost:5100/logout');
+          history.push("/");
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
+  useEffect(() => {
+    refreshToken();
+},[]);
+
+const refreshToken = async () => {
+    try {
+        const response = await axios.get('http://localhost:5100/token');
+        setToken(response.data.accessToken);
+        const decoded = jwt_decode(response.data.accessToken);
+        setName(decoded.name);
+        setExpire(decoded.exp);
+    } catch (error) {
+        if (error.response) {
+            history.push("/");
+        }
+    }
+}
+
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+          const response = await axios.get('http://localhost:5100/token');
+          config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+          setToken(response.data.accessToken);
+          const decoded = jwt_decode(response.data.accessToken);
+          setName(decoded.name);
+          setExpire(decoded.exp);
+      }
+      return config;
+  }, (error) => {
+      return Promise.reject(error);
+  });
 
   return (
     <>
@@ -24,14 +74,14 @@ const AdminNavbar = () => {
             <Nav>
               <Navbar.Text>
                 <p className="hsatu">
-                  <strong>Tiara</strong> - Petugas Entry
+                  <strong>{name}</strong> - Petugas Entry
                 </p>
               </Navbar.Text>
             </Nav>
 
             <IoLogOutOutline
               className="nav-logout"
-              onClick={() => navigate(HOME)}
+              onClick={Logout}
               style={{ color: "white" }}
               size="27px"
             />
